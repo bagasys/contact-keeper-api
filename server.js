fc3c7757213eaa66369fd97c508b238287c60368
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-11-17 21:22:54 +0700

    adding inital routes
    auth, contacts, users

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    return res.json({msg: "welcome to contact keeper API"});
+});
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
